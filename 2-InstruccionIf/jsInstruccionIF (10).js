c9dbcf82a2a0aa39261fc91f9f8dbf090d50db08
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-15 11:55:59 -0300

    Update jsInstruccionIF (10).js

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var nota = Math.floor(Math.random() * 11);
 	
+		if(nota == 9 || nota == 10){
+			alert("Excelente");
+		}
+		else if(nota < 4){
+			alert("Vamos!");
+		}
+		else{
+			alert("Aprobo");
+		}
+
 
 }//FIN DE LA FUNCIÓN
