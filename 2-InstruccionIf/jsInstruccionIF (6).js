faaedb8202e81733c3b7ddef60dcedee9fc539ba
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-15 10:29:39 -0300

    Update jsInstruccionIF (6).js

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -3,6 +3,17 @@ function Mostrar()
 //tomo la edad  
 
 
+    var edad = parseInt(document.getElementById("edad").value);
+
+    if(edad >= 18){
+        alert("Adulto");
+    }
+    else if(edad < 13){
+        alert("Niño");
+    }
+    else{
+        alert("Adolescente");
+    }
 
 
 
