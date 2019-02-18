5e6ece8672286e85dc9e4db9790e1b5137c9f89d
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-18 11:31:49 -0300

    Update jsInstruccionSwitch(4).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,7 +5,23 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+switch(mesDelAño){
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Dicembre":
+        alert("31 dias" );
+        break;
+
+    case "Febrero":
+        alert("28 dias");
+        break;
+    default:
+        alert("30 dias");
+}	
 
 
 
