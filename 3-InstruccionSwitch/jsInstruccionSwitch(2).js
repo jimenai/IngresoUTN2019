da7a1e190201576d80c869a79ec63a215e053d29
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-18 11:24:32 -0300

    Update jsInstruccionSwitch(2).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -6,38 +6,20 @@ var mesDelAño = document.getElementById('mes').value;
 alert (mesDelAño);
 switch(mesDelAño){
     case "Enero":
-        alert("Ya pasamos el frio, ahora calor!!!");
-        break;
     case "Febrero":
-        alert("Ya pasamos el frio, ahora calor!!!");
-        break;
     case "Marzo":
-        alert("Falta para el invierno." );
-        break;
     case "Abril":
-        alert("Falta para el invierno." );
-        break;
     case "Mayo":
-        alert("Falta para el invierno." );
-        break;
     case "Junio":
         alert("Falta para el invierno." );
          break;
     case "Julio":
-        alert("Abrigate! hace frio...");
-        break;
     case "Agosto":
         alert("Abrigate! hace frio...");
         break;
     case "Septiembre":
-        alert("Ya pasamos el frio, ahora calor!!!");
-         break;
     case "Octubre":
-        alert("Ya pasamos el frio, ahora calor!!!." );
-        break;
     case "Noviembre":
-        alert("Ya pasamos el frio, ahora calor!!!." );
-        break;
     case "Dicembre":
         alert("Ya pasamos el frio, ahora calor!!!." );
         break;
