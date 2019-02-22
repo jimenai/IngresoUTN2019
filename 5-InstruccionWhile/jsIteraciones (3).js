19771ddb23379f696e9201fbfe38f1388b5661fa
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-22 09:35:52 -0300

    Update jsIteraciones (3).js

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -4,4 +4,11 @@ function Mostrar()
 var clave = prompt("ingrese el número clave.");
 
 
+    while(clave != "utn750"){
+        clave = prompt("Clave Incorrecta. Ingrese nueva clave por favor.");
+    }
+
+    alert("Clave correcta");
+
+
 }//FIN DE LA FUNCIÓN
