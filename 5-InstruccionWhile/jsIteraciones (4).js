54f671171e8c0d6544cb1a236e806403257ec806
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-22 09:34:27 -0300

    Update jsIteraciones (4).js

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -3,5 +3,11 @@ function Mostrar()
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
 
+	while(!(numero>=0 && numero<10)){
+        numero = prompt("El numero elegido debe estar comprendido entre 0 y 9. Por favor reingrese el numero");
+   	}
+
+    alert("Numero");
+
 
 }//FIN DE LA FUNCIÓN
