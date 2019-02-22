ad1e2a55a8d5ce72c83a3901c60d5e1c7092345d
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-22 10:28:08 -0300

    Update jsIteraciones (5).js

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,9 +2,19 @@ function Mostrar()
 {
 
 var sexo = prompt("ingrese f ó m .");
+  while(!(sexo == "m" || sexo == "f")){
+        sexo = prompt("Sexo Incorrecto. Reingrese f o m, segun su sexo.");
+    }
 
+    if(sexo == "f"){
+        sexo = "Femenino";
+        document.getElementById('Sexo').value=sexo;
+    }
+    else{
+        sexo = "Masculino";
+        document.getElementById('Sexo').value=sexo;
+    }
+    
 
-
-document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
