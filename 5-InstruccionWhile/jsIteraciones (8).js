8119b6632e5c1ecdcac8a694a3a9c5b4e72db76a
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-22 23:41:38 -0300

    Update jsIteraciones (8).js

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,37 @@
 function Mostrar()
 {
 
-	var contador=0;
+	var numero;
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
+	var respuestaUsuario = "no";
+	var numeroIngresado;
+	var ingresoNumeroNegativo = false;
+	do{
+		numeroIngresado = prompt("Ingrese numero:");
+		if(!isNaN(numeroIngresado)){
+			numero = parseInt(numeroIngresado);
+			if(numero>0){
+				positivo = positivo + numero;
+			}
+			else{
+				negativo= negativo * numero;
+				ingresoNumeroNegativo = true;
+			}
+			respuestaUsuario = prompt("¿Desea agregar otro numero? Des ser correcto responda Si, de lo contraio No").toLowerCase();
+		}
+		else{
+			alert("Solo se permiten números");
+			continue;
+		}
+		
+		
+	} while(respuesta == respuestaUsuario);
 
+	document.getElementById('suma').value=positivo;
+	if(ingresoNumeroNegativo)
+		document.getElementById('producto').value=negativo;
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
 
 }//FIN DE LA FUNCIÓN
