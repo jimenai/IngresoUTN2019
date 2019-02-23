3974890fb5099d98d0b602679de72706c538e1a0
jimenai <47321446+jimenai@users.noreply.github.com>
2019-02-22 22:39:28 -0300

    Update jsIteraciones (7).js

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -3,10 +3,18 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta="si";
+	var numero;
+	var respuestaUsuario;
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	do{
+		numero = parseInt(prompt("Ingrese un numero"));
+		acumulador = acumulador + numero;
+		contador++;
+		respuestaUsuario = prompt("¿Desea agregar otro numero?. De ser correcto responda Si, de lo contrario No.").toLowerCase();
+	} while(respuesta == respuestaUsuario);
+	
+	document.getElementById('suma').value=acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
