c151b9a2bb5bf433a02a6d78d8489ec653057a29
jimenai <47321446+jimenai@users.noreply.github.com>
2019-03-01 09:46:34 -0300

    Update jsIteracionesFor (6).js

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,20 @@
 function Mostrar()
 {
 
+    var numero = parseInt(prompt("Ingrese un numero"));
+    var contadorPar = 0;
+    
+    while(numero <= 0 || isNaN(numero)){
+        numero = parseInt(parseFloat("Error. Ingrese un numero"));
+    }
 
+    for(var i=1; i<= numero; i++){
+        if(i %2 == 0){
+           console.log(i);
+           contadorPar ++;
+        }
+    }
+    console.log("Cantidad numero pares: "+ contadorPar);
 
 
 }//FIN DE LA FUNCIÓN
