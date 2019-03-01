8a8c701c632ba93505f13dc69df1a261974db0e1
jimenai <47321446+jimenai@users.noreply.github.com>
2019-03-01 10:00:19 -0300

    Update jsIteracionesFor (7).js

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,5 +1,22 @@
 function Mostrar()
 {
+  var numero = parseInt(prompt("Ingrese un numero"));
+    var contadorDivisores = 0;
+    
+    while(numero <= 0 || isNaN(numero)){
+        numero = parseInt(parseFloat("Error. Ingrese un numero"));
+    }
+    
+    for(var i=1; i<= numero; i++){
+        if(numero %i == 0 ){
+           console.log(i);
+           contadorDivisores ++;
+        }
+    }
+    
+    console.log("Cantidad divisores: "+ contadorDivisores);
+
+
 
 
 
